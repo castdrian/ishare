613d061618c09901e10ea19dd88113ca53833087
castdrian <adrifcastr@gmail.com>
2023-07-10 20:27:44 +0200

    Initial Commit

---
+++ ishare/ishareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ishareApp.swift
+//  ishare
+//
+//  Created by Adrian Castro on 10.07.23.
+//
+
+import SwiftUI
+
+@main
+struct ishareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
