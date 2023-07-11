81c63df5c312f429052ba2ba939c87295b8354e5
castdrian <adrifcastr@gmail.com>
2023-07-11 21:31:58 +0200

    implementation about panel

--- ishare/MenuBarScene.swift
+++ ishare/MenuBarScene.swift
@@ -7,36 +7,56 @@
 
 import SwiftUI
 
+enum PostCaptureTasks: String, CaseIterable, Identifiable {
+    case COPY_TO_CLIPBOARD, OPEN_CAPTURE_FOLDER, UPLOAD_CAPTURE
+    var id: Self { self }
+}
+
 enum Destination: String, CaseIterable, Identifiable {
-    case imgur, custom
+    case IMGUR, CUSTOM
     var id: Self { self }
 }
 
 struct MenuBarScene: Scene {
-    @State private var selectedDestination: Destination = .imgur
+    @State private var selectedDestination: Destination = .IMGUR
 
     var body: some Scene {
         MenuBarExtra("ishare", systemImage: "photo.on.rectangle.angled") {
             Menu("Capture") {
                 Button("Region Capture") {}
                 Button("Window Capture") {
-                    captureScreen(options: CaptureOptions(filePath: nil, type: CaptureType.WindowImage, ext: FileType.PNG, saveFileToClipboard: true))
+                  //  captureScreen(options: CaptureOptions(filePath: nil, type: CaptureType.WindowImage, ext: FileType.PNG, saveFileToClipboard: true))
                 }
                 Button("Screen Capture") {}
                 Button("Record Region") {}.disabled(true)
                 Button("Record Screen") {}.disabled(true)
             }
-            Menu("Upload") {}.disabled(true)
-            Menu("History") {}.disabled(true)
 
             Picker("Destination", selection: $selectedDestination) {
-                Text("Imgur")
-                Text("Custom")
+                ForEach(Destination.allCases, id: \.self) {
+                    Text($0.rawValue.capitalized)
+                }
             }.pickerStyle(MenuPickerStyle())
             
             Button("Settings") {}.keyboardShortcut("s").disabled(true)
             Divider()
-            Button("About") {}.keyboardShortcut("a").disabled(true)
+            Button("About ishare") {
+                NSApplication.shared.activate(ignoringOtherApps: true)
+                NSApplication.shared.orderFrontStandardAboutPanel(
+                    options: [
+                        NSApplication.AboutPanelOptionKey.credits: NSAttributedString(
+                            string: "isharemac.app",
+                            attributes: [
+                                NSAttributedString.Key.font: NSFont.boldSystemFont(
+                                    ofSize: NSFont.smallSystemFontSize)
+                            ]
+                    ),
+                        NSApplication.AboutPanelOptionKey(
+                            rawValue: "Copyright"
+                        ): "© 2023 ADRIAN CASTRO"
+                    ]
+                )
+            }.keyboardShortcut("a")
             Button("Quit") {
                 NSApplication.shared.terminate(nil)
             }.keyboardShortcut("q")
