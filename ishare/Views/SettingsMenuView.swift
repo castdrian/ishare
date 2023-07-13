c3879982ea5e281afc03565b65e3ce79e94bdd4f
castdrian <adrifcastr@gmail.com>
2023-07-13 20:04:48 +0200

    simulate ffmpeg install

--- ishare/Views/SettingsMenuView.swift
+++ ishare/Views/SettingsMenuView.swift
@@ -14,34 +14,34 @@ struct SettingsMenuView: View {
     var body: some View {
         TabView {
             GeneralSettingsView()
-            .tabItem {
-                Label("General", systemImage: "gear")
-            }
-                
+                .tabItem {
+                    Label("General", systemImage: "gear")
+                }
+            
             UploaderSettingsView()
-            .tabItem {
-                Label("Uploaders", systemImage: "icloud.and.arrow.up")
-            }
-                
+                .tabItem {
+                    Label("Uploaders", systemImage: "icloud.and.arrow.up")
+                }
+            
             KeybindSettingsView()
-            .tabItem {
-                Label("Keybinds", systemImage: "command.circle")
-            }
+                .tabItem {
+                    Label("Keybinds", systemImage: "command.circle")
+                }
             
             CaptureSettingsView()
-            .tabItem {
-                Label("Captures", systemImage: "photo")
-            }
+                .tabItem {
+                    Label("Captures", systemImage: "photo")
+                }
             
             RecordingSettingsView()
-            .tabItem {
-                Label("Recordings", systemImage: "menubar.dock.rectangle.badge.record")
-            }
+                .tabItem {
+                    Label("Recordings", systemImage: "menubar.dock.rectangle.badge.record")
+                }
             
             AdvancedSettingsView()
-            .tabItem {
-                Label("Advanced", systemImage: "hammer.circle")
-            }
+                .tabItem {
+                    Label("Advanced", systemImage: "hammer.circle")
+                }
         }
         .frame(width: 550, height: 350)
     }
@@ -52,14 +52,14 @@ struct GeneralSettingsView: View {
         LaunchAtLogin.Toggle()
     }
 }
- 
+
 struct UploaderSettingsView: View {
     var body: some View {
         Text("Custom Uploader Settings")
             .font(.title)
     }
 }
- 
+
 struct KeybindSettingsView: View {
     var body: some View {
         Form {
@@ -101,9 +101,77 @@ struct CaptureSettingsView: View {
 }
 
 struct RecordingSettingsView: View {
+    @State private var showingAlert: Bool = false
+    @State private var isInstalling = false
+    @State private var installProgress: Double = 0.0
+    
+    private var alertTitle: String = "Install ffmpeg"
+    private var alertMessage: String = "Do you want to install ffmpeg on this Mac?"
+    private var alertButtonText: String = "Confirm"
+    
     var body: some View {
-        Text("Recording Settings")
-            .font(.title)
+        VStack {
+            HStack {
+                Text("ffmpeg status:")
+                Button(isFFmpegInstalled() ? "installed" : "not installed") {
+                    showingAlert = true
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(isFFmpegInstalled() ? .green : .pink)
+                .disabled(isFFmpegInstalled())
+                .alert(Text(alertTitle),
+                       isPresented: $showingAlert,
+                       actions: {
+                    Button(alertButtonText) {
+                        showingAlert = false
+                        installFFmpeg()
+                    }
+                    Button("Cancel", role: .cancel) {
+                        showingAlert = false
+                    }
+                }, message: {
+                    Text(alertMessage)
+                }
+                )
+            }
+            if isInstalling {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(Color(NSColor.windowBackgroundColor))
+                    .frame(width: 200, height: 180)
+                    .overlay(
+                        VStack {
+                            Text("Installing FFmpeg...")
+                                .foregroundColor(.primary)
+                            Text(String(format: "%.0f%%", installProgress))
+                                .font(.title2)
+                                .foregroundColor(.primary)
+                                .padding(.bottom, 8)
+                            ProgressView(value: installProgress, total: 100)
+                                .progressViewStyle(LinearProgressViewStyle())
+                                .accentColor(.blue)
+                                .padding(.horizontal)
+                        }
+                            .padding()
+                    )
+            }
+        }
+    }
+    func installFFmpeg() {
+        isInstalling = true
+        
+        DispatchQueue.global().async {
+            // Simulating installation progress for demonstration purposes
+            let totalProgress: Double = 100.0
+            let increment: Double = 1.0
+            
+            while installProgress < totalProgress {
+                installProgress += increment
+                usleep(20000) // Simulating delay between progress updates
+            }
+            
+            // Install completed
+            isInstalling = false
+        }
     }
 }
 
