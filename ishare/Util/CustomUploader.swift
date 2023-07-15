7d470a07fea6f1b3cd528534e6dc6f112a406f21
castdrian <adrifcastr@gmail.com>
2023-07-15 13:14:01 +0200

    add custom uploader stuff

---
+++ ishare/Util/CustomUploader.swift
@@ -0,0 +1,59 @@
+//
+//  CustomUploader.swift
+//  ishare
+//
+//  Created by Adrian Castro on 15.07.23.
+//
+
+import Foundation
+import Defaults
+
+struct CustomUploader: Codable, Defaults.Serializable {
+    let name: String
+    let requestUrl: String
+    let headers: [String: String]?
+    let formData: [String: String]?
+    let responseProp: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case requestUrl
+        case headers
+        case formData
+        case responseProp
+    }
+    
+    static func fromJSON(_ json: Data) throws -> CustomUploader {
+        let decoder = JSONDecoder()
+        return try decoder.decode(CustomUploader.self, from: json)
+    }
+    
+    func toJSON() throws -> Data {
+        let encoder = JSONEncoder()
+        return try encoder.encode(self)
+    }
+    
+    func isValid() -> Bool {
+        guard !requestUrl.isEmpty else {
+            return false
+        }
+        
+        if let headers = headers {
+            guard headers as Codable is [String: String] else {
+                return false
+            }
+        }
+        
+        if let formData = formData {
+            guard formData as Codable is [String: String] else {
+                return false
+            }
+        }
+        
+        guard !responseProp.isEmpty else {
+            return false
+        }
+        
+        return true
+    }
+}
