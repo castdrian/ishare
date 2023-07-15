75ac29b03b72ef15b6b26a21b44b587fd28fb840
castdrian <adrifcastr@gmail.com>
2023-07-15 10:39:04 +0200

    cleanup imgur

--- ishare/Http/Imgur.swift
+++ ishare/Http/Imgur.swift
@@ -11,27 +11,13 @@ import Defaults
 import AppKit
 
 func imgurUpload(_ fileURL: URL, completion: @escaping () -> Void) {
-    guard let base64Image = imageToBase64(fileURL) else {
-        print("Failed to convert image to base64")
-        return
-    }
-    
     @Default(.captureFileType) var fileType
     @Default(.imgurClientId) var imgurClientId
     
     let url = "https://api.imgur.com/3/upload"
     
     AF.upload(multipartFormData: { multipartFormData in
-        if let imageData = Data(base64Encoded: base64Image) {
-            multipartFormData.append(imageData, withName: "image", fileName: "ishare.\(fileType)", mimeType: "image/\(fileType)")
-        }
-        
-        let parameters = ["image": base64Image]
-        for (key, value) in parameters {
-            if let data = value.data(using: .utf8) {
-                multipartFormData.append(data, withName: key)
-            }
-        }
+        multipartFormData.append(fileURL, withName: "image", fileName: "ishare.\(fileType)", mimeType: "image/\(fileType)")
     }, to: url, method: .post, headers: ["Authorization": "Client-ID " + imgurClientId]).response { response in
         if let data = response.data,
            let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any],
@@ -48,17 +34,5 @@ func imgurUpload(_ fileURL: URL, completion: @escaping () -> Void) {
             print("Error parsing response or retrieving image link")
             completion()
         }
-    }
-}
-
-
-func imageToBase64(_ fileURL: URL) -> String? {
-    do {
-        let imageData = try Data(contentsOf: fileURL)
-        let base64String = imageData.base64EncodedString()
-        return base64String
-    } catch {
-        print("Error reading file: \(error)")
-        return nil
     }
 }
