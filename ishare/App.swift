ed30f7eb5204605d4a74249522d09c45f09b2a45
castdrian <adrifcastr@gmail.com>
2023-07-22 20:19:46 +0200

    cleanup

--- ishare/App.swift
+++ ishare/App.swift
@@ -34,12 +34,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         if urls.count == 1 {
             importIscu(urls.first!)
         }
-    }
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Show extensions, if ishare is not approved
-        if !FIFinderSyncController.isExtensionEnabled {
-            FIFinderSyncController.showExtensionManagementInterface()
-        }
-    }
+    }    
 }
